6c890b332c78c0a517972ccb2ffb027384c5e02a
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2024-06-11 17:59:12 +0900

    Create main.java

---
+++ JavaLevel3/main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class PokemomMain {
+	
+	public static void main(String[] args) {
+	
+		Pokemon 콩둘기 = new Pokemon();
+		Pokemon 지그제구리 = new Pokemon();
+		
+		콩둘기.능력치();
+		지그제구리.능력치();
+	}
+	
+}
