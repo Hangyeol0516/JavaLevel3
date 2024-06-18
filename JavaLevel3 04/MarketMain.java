f61ae8760a491abfdbdd516ffd4cc1c302c43945
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2024-06-18 17:41:17 +0900

    Create MarketMain.java

---
+++ JavaLevel3 04/MarketMain.java
@@ -0,0 +1,15 @@
+package market;
+
+public class MarketMain {
+
+	public static void main(String[] args) {
+	
+		Market m1 = new Market("지현맘마켓");
+		
+		System.out.println(m1);
+		System.out.println(m1.마켓이름);
+		System.out.println(m1.상품[0].get상품명());
+		System.out.println(Item.상품명번호);
+	}
+	
+}
